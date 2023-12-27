ba12f719f0c7a64462d039eb74bb6d5f62afc7dc
gstbgsdarmika <gstbgsdarmika@gmail.com>
2023-12-27 13:21:49 +0800

    update ml to web

---
+++ backend/module/AnalysisService.py
@@ -0,0 +1,68 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, classification_report
+
+class TextClassifier:
+    def __init__(self):
+        self.tfidf_vectorizer = TfidfVectorizer()
+        self.chi2_selector = None
+        self.rf_classifier = None
+
+    def preprocess_text(self, text_data):
+        # You can add your text preprocessing logic here
+        # For now, assuming 'text_data' is already preprocessed
+        return text_data
+
+    def train_tfidf(self, text_data):
+        preprocessed_text = self.preprocess_text(text_data)
+        tfidf = self.tfidf_vectorizer.fit_transform(preprocessed_text)
+        return tfidf
+
+    def select_features_chi2(self, tfidf, labels):
+        kbest = int(0.4 * tfidf.shape[1])
+        self.chi2_selector = SelectKBest(chi2, k=kbest)
+        X_new_aspek = self.chi2_selector.fit_transform(tfidf, labels)
+        return X_new_aspek
+
+    def train_rf(self, x, y):
+        # splitting
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+        # Define the Random Forest classifier
+        self.rf_classifier = RandomForestClassifier()
+
+        # Define the hyperparameter grid for tuning
+        param_grid = {
+            'n_estimators': [50, 100, 200],
+            'max_depth': [None, 10, 20, 30],
+            'min_samples_split': [2, 5, 10],
+            'min_samples_leaf': [1, 2, 4]
+        }
+
+        # Use GridSearchCV for hyperparameter tuning and cross-validation
+        grid_search = GridSearchCV(estimator=self.rf_classifier, param_grid=param_grid, cv=5, scoring='accuracy')
+        grid_search.fit(X_train, y_train)
+
+        # Access Accuracy Scores for Each Combination
+        results = grid_search.cv_results_
+
+        # Print accuracy scores for each combination
+        for mean_score, params in zip(results['mean_test_score'], results['params']):
+            print(f"Accuracy: {mean_score:.4f}, Parameters: {params}")
+
+        # Get the best parameters and the best model
+        best_params = grid_search.best_params_
+        best_rf_model = grid_search.best_estimator_
+
+        # Evaluate the model on the test set
+        y_pred = best_rf_model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+
+        # # Display results
+        # print("\nJumlah fitur:", x.shape[1])
+        # print("Best Hyperparameters:", best_params)
+        # print("Test Accuracy:", accuracy)
+        # print("\nClassification Report:\n", classification_report(y_test, y_pred))
+        # return best_rf_model
--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS  
 from models import db
 from routes import register, login
+from module.Preprocessing import TextProcessor
+from module.AnalysisService import TextClassifier
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -15,6 +18,45 @@ db.init_app(app)
 # Register Blueprints
 app.register_blueprint(register)
 app.register_blueprint(login)
+
+# Create an instance of TextProcessor and TextClassifier
+text_processor = TextProcessor()
+text_classifier = TextClassifier()
+
+# Define allowed file extensions
+ALLOWED_EXTENSIONS = {'csv'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/analisis', methods=['POST'])
+def upload_csv():
+    try:
+        file = request.files['file']
+
+        if file and allowed_file(file.filename):
+            # Read CSV into a DataFrame
+            df = pd.read_csv(file)
+
+            # Preprocessing using the TextProcessor
+            df['preprocessed'] = df['Text Tweet'].apply(text_processor.preprocess_all)
+            
+            # Extract features using the TextClassifier
+            tfidf_matrix = text_classifier.train_tfidf(df["preprocessed"])
+            selected_features = text_classifier.select_features_chi2(tfidf_matrix, df['Pasangan Calon'])
+
+            # Train the Random Forest classifier
+            best_rf_model = text_classifier.train_rf(selected_features, df['Pasangan Calon'])
+
+            # Predict using the trained model
+            df['prediction'] = best_rf_model.predict(selected_features)
+
+            # Return the processed DataFrame with predictions as JSON
+            return jsonify(df.to_dict(orient='records'))
+        else:
+            return jsonify({'error': 'Invalid file or file type'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 # Running app
 if __name__ == '__main__':
